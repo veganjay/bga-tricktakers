ce493656f6719bf19c54515dafdf7241971a8105
Jason Youzwak <jyouzwak@peratonlabs.com>
2024-06-05 19:27:34 -0400

    Refactor to one file

--- create_tricktakers_csv.py
+++ create_tricktakers_csv.py
@@ -2,7 +2,13 @@
 
 import json
 import csv
+import re
+import os
+import requests
 from typing import List, Tuple, Dict
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 
 def list_to_ranges(numbers: List[int]) -> List[Tuple[int, int]]:
@@ -21,12 +27,10 @@ def list_to_ranges(numbers: List[int]) -> List[Tuple[int, int]]:
     """
     if not numbers:
         return []
-
     numbers.sort()
     ranges = []
     start = numbers[0]
     end = numbers[0]
-
     for number in numbers[1:]:
         if number == end + 1:
             end = number
@@ -34,23 +38,8 @@ def list_to_ranges(numbers: List[int]) -> List[Tuple[int, int]]:
             ranges.append((start, end))
             start = number
             end = number
-
     ranges.append((start, end))
     return ranges
-
-
-def read_data_json(filename: str) -> Dict:
-    """
-    Reads JSON data from a file and returns it as a dictionary.
-
-    Args:
-    filename: Path to the JSON file.
-
-    Returns:
-    Dictionary with the data from the JSON file.
-    """
-    with open(filename, 'r') as fp:
-        return json.load(fp)
 
 
 def format_range(player_range: Tuple[int, int]) -> str:
@@ -64,11 +53,7 @@ def format_range(player_range: Tuple[int, int]) -> str:
     Formatted string representation of the range.
     """
     min_players, max_players = player_range
-    return (
-        f'{min_players}-{max_players}'
-        if min_players != max_players
-        else str(min_players)
-    )
+    return f'{min_players}-{max_players}' if min_players != max_players else str(min_players)
 
 
 def format_range_list(player_range_list: List[Tuple[int, int]]) -> str:
@@ -81,9 +66,7 @@ def format_range_list(player_range_list: List[Tuple[int, int]]) -> str:
     Returns:
     Formatted string representation of the list of ranges.
     """
-    return ' or '.join(
-        format_range(player_range) for player_range in player_range_list
-    )
+    return ' or '.join(format_range(player_range) for player_range in player_range_list)
 
 
 def is_tricktaker(tag_tuple_list: List[Tuple[int, int]]) -> bool:
@@ -112,13 +95,9 @@ def process_data(data: List[Dict]) -> List[Tuple[str, str, str]]:
     processed_data = []
     for item in data:
         name = item.get('display_name_en')
-        player_numbers = format_range_list(
-            list_to_ranges(item.get('player_numbers'))
-        )
+        player_numbers = format_range_list(list_to_ranges(item.get('player_numbers')))
         tags = item.get('tags')
-        status = (
-            'alpha' if item.get('status') == 'private' else item.get('status')
-        )
+        status = 'alpha' if item.get('status') == 'private' else item.get('status')
         if is_tricktaker(tags):
             processed_data.append((name, status, player_numbers))
     return processed_data
@@ -142,9 +121,52 @@ def write_to_csv(data: List[Tuple[str, str, str]], filename: str) -> None:
 
 
 def main():
-    data = read_data_json("data/bga-list.json")
-    processed_data = process_data(data)
-    write_to_csv(processed_data, 'bga-tricktakers.csv')
+    # Load env variables
+    load_dotenv()
+
+    # Step 1: Retrieve HTML file by URL
+    url = 'https://boardgamearena.com'
+    
+    # Load cookies from .env file
+    cookies = {
+        'PHPSESSID': os.getenv('PHPSESSID'),
+        'TournoiEnLigne_sso_id': os.getenv('TournoiEnLigne_sso_id'),
+        'TournoiEnLigne_sso_user': os.getenv('TournoiEnLigne_sso_user'),
+        'TournoiEnLigneid': os.getenv('TournoiEnLigneid'),
+        'TournoiEnLigneidt': os.getenv('TournoiEnLigneidt'),
+        'TournoiEnLignetk': os.getenv('TournoiEnLignetk'),
+        'TournoiEnLignetkt': os.getenv('TournoiEnLignetkt'),
+        '__stripe_mid': os.getenv('__stripe_mid'),
+    }
+    
+    response = requests.get(url, cookies=cookies)
+    html_content = response.text
+    
+    # Step 2: Find the JavaScript <script> section
+    soup = BeautifulSoup(html_content, 'html.parser')
+    scripts = soup.find_all('script')
+    
+    # Step 3: Obtain the value for the variable globalUsersInfo
+    global_users_info = None
+    for script in scripts:
+        if 'globalUserInfos' in script.text:
+            # Use regular expression to extract the value of globalUsersInfo
+            match = re.search(r'globalUserInfos\s*=\s*({.*?});', script.text, re.DOTALL)
+            if match:
+                global_users_info = match.group(1)
+                break
+    
+    global_users_info_dict = json.loads(global_users_info)
+    
+    # Process and write data to CSV
+    game_list = global_users_info_dict.get('game_list')
+    processed_data = process_data(game_list)
+
+    # Create a filename
+    filename = 'bga-tricktakers.csv'
+
+    print(f'Writing to {filename}')
+    write_to_csv(processed_data, filename)
 
 
 if __name__ == '__main__':
